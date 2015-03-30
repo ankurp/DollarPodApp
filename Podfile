fcbd1e56c9a565f9e51d756e5dd3931e8bfc39b4
Ankur Patel <ankur.patel@ymail.com>
2015-03-29 20:11:11 -0400

    Updating gitignore

---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '8.0'
+
+pod 'Dollar'
+pod 'Cent'
+
+use_frameworks!
---
+++ Pods/Target Support Files/Pods-Cent/Pods-Cent-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Cent : NSObject
+@end
+@implementation PodsDummy_Pods_Cent
+@end
